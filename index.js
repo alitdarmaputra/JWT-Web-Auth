33acd303aa23bbb5e69dfa22ba3c3bdfd1271b3c
alitdarmaputra <alitdarmaputra@gmail.com>
2022-07-14 20:52:30 +0800

    Add user interface

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 require("dotenv").config();
-
 const express = require("express");
+
 const app = express();
+app.set("view engine", "ejs");
+
+// Route
 
 app.get("/", (req, res) => {
-    res.json("Hello");
+    res.render("homepage");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
 });
 
 let port = 4000;
